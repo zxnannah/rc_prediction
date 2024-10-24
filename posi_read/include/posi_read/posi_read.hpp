ba1cdacafef90621fc1e3504f16ac70bd134d292
nannah <18066721353@163.com>
2024-10-25 05:25:43 +0800

    ekf-first-commit

--- posi_read/include/posi_read/posi_read.hpp
+++ posi_read/include/posi_read/posi_read.hpp
@@ -10,26 +10,19 @@
 
 class BallCsvReaderNode : public rclcpp::Node {
 public:
-    // 构造函数声明
     BallCsvReaderNode();
 
 private:
-    // CSV 读取函数声明
     void loadCsvData(const std::string& file_path);
 
-    // 发布数据点的函数声明
     void publishNextPoint();
-
-    // 3D 点结构体
     struct Point3D {
         double x, y, z;
     };
 
-    // 数据存储向量
     std::vector<Point3D> data_;
-    size_t index_ = 0;  // 当前读取的索引
+    size_t index_ = 0;
 
-    // ROS2 发布者和定时器
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
